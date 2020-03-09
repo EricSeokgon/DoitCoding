ab91250b0daf2a95f628821f0be3ffe17e851849
EricSeokgon <leesk55@gmail.com>
2020-03-09 12:02:01 +0900

    생성자 만들기

--- src/elice/day2/세면도구세트.java
+++ src/elice/day2/세면도구세트.java
@@ -1,9 +1,21 @@
 package elice.day2;
 
-public class 세면도구세트 {
+class 세면도구세트{
     int 칫솔;
     int 치약;
     int 비누;
     int 샴푸;
     int 린스;
+
+    세면도구세트(int A, int B, int C, int D, int E){
+        칫솔 = A;
+        치약 = B;
+        비누 = C;
+        샴푸 = D;
+        린스 = E;
+    }
+
+    public 세면도구세트() {
+
+    }
 }
