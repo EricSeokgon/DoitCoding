d56d9f935d2f7b9282c1e85be9f4e5c82d44902f
EricSeokgon <leesk55@gmail.com>
2020-03-09 12:51:58 +0900

    this 사용하기

--- src/elice/day2/세면도구세트.java
+++ src/elice/day2/세면도구세트.java
@@ -7,12 +7,20 @@ class 세면도구세트{
     int 샴푸;
     int 린스;
 
-    세면도구세트(int A, int B, int C, int D, int E){
+    /*세면도구세트(int A, int B, int C, int D, int E){
         칫솔 = A;
         치약 = B;
         비누 = C;
         샴푸 = D;
         린스 = E;
+    }*/
+
+    세면도구세트(int 칫솔, int 치약, int 비누, int 샴푸, int 린스){
+        this.칫솔 = 칫솔;
+        this.치약 = 치약;
+        this.비누 = 비누;
+        this.샴푸 = 샴푸;
+        this.린스 = 린스;
     }
 
     public 세면도구세트() {
